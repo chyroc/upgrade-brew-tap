5532da6f206818ff093c26c46873474f0ab0e207
chyroc <chyroc@qq.com>
2021-09-30 23:16:21 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,18 @@ env
 tag_v="${GITHUB_REF#refs/*/}"
 tag="0.3.0" # `echo $tag_v | cut -c2-`
 name="$INPUT_NAME"
-repo="otp-cli" # "$GITHUB_REPOSITORY"
+tap_repo="$INPUT_TAP_REPO"
+repo="chyroc/otp-cli" # "$GITHUB_REPOSITORY"
 
 checksum_url="https://github.com/$repo/releases/download/v$tag/checksums.txt"
 checksum=`curl -sL $checksum_url | grep $name-$tag.tar.gz | cut -d ' '  -f 1-1 | xargs`
 
+echo "name: $name"
 echo "tag: $tag"
 echo "checksum: $checksum"
-echo "name: $name"
+echo "tap_repo: $tap_repo"
 
 echo "::set-output name=name::$name"
 echo "::set-output name=tag::$tag"
 echo "::set-output name=checksum::$checksum"
+echo "::set-output name=tap_repo::$tap_repo"
